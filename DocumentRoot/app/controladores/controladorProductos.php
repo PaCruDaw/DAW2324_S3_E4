b44eade6ddeb9cf3f75ee173491eb43e31dfafce
joseproldan <joseproldan@iesmontsia.org>
2023-11-28 17:50:02 +0100

    mvc Products

--- DocumentRoot/app/controladores/controladorProductos.php
+++ DocumentRoot/app/controladores/controladorProductos.php
@@ -1,36 +1,14 @@
 <?php
-session_start();
-
-// Incluir el modelo de productos
 include('../modelos/modeloProducto.php');
 
-// Crear una instancia de la clase Product
-$product = new Product();
-
-
 // Manejar la actualización de productos
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['update']) && isset($_POST['id_producto'])) {
-    $id_producto = $_POST['id_producto'];
-    $nombre_producto = $_POST['nombre_producto'];
-    $descripcion_producto = $_POST['decripcion_producto'];
-    $precio = $_POST['precio'];
-    $margen_porcentaje = $_POST['margen_porcentaje'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nuevoMargen'])) {
+    $id = $_POST['id'];
+    $nuevoMargen = $_POST['nuevoMargen'];
 
     // Llamar al método de actualización del modelo
-    $result = $product->updateProduct($id_producto);
+    $product->updateProduct($id, $nuevoMargen);
 
-    if ($result) {
-        // Producto actualizado exitosamente
-        echo("funciono");
-        header('Location: ./vistas/vistaProductos.php'); // Redirigir a la misma página después de la actualización
-        exit();
-    } else {
-    // Ocurrió un error al actualizar el producto
-        echo "Error al actualizar el producto.";
-    }
 }
-
-
-
-
+$products = $product->getProducts();
 ?>
