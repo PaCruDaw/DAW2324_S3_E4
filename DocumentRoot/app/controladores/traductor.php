7d5ab11b75bb122e4c07612a95ab1d5609425d0a
michipce <michipce@gmail.com>
2023-12-13 14:49:09 +0100

    inici cookie d'inici i traductor canvi

--- DocumentRoot/app/controladores/traductor.php
+++ DocumentRoot/app/controladores/traductor.php
@@ -2,31 +2,10 @@
 require_once '/var/www/html/modelos/modelotraducciones.php';
 
 if (isset($_GET['lang'])) {
-    $lang = $_GET['lang'];
-    setcookie('lang', $lang, time() + 86400, "/"); // Utiliza $lang en lugar de $nuevoIdioma
-} else {
-    if (!isset($_COOKIE['lang'])) {
-        $userLanguages = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-        $configLangUser = explode(',', $userLanguages);
-        $userLang = $configLangUser[0];
-        if ($userLang == "es-ES") {
-            setcookie('lang', 'ESP', time() + 86400, "/");
-            $lang = 'ESP';
-        } elseif ($userLang == "ca-CA") {
-            setcookie('lang', 'CAT', time() + 86400, "/");
-            $lang = 'CAT';
-        } elseif ($userLang == "it-IT") {
-            setcookie('lang', 'ITA', time() + 86400, "/");
-            $lang = 'ITA';
-        } else {
-            setcookie('lang', 'ENG', time() + 86400, "/");
-            $lang = 'ENG';
-        }
-    } else {
-        $lang = $_COOKIE['lang'];
-
-    }
+    setcookie('lang', $_GET['lang'], time() + 86400, "/"); // Utiliza $lang en lugar de $nuevoIdioma
 }
+
+$lang = $_COOKIE['lang'];
 
 //static class for translate
 class TranslatePage {
