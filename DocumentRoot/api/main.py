3ae5369a31ab27f96b243565bc7d7a4a9733af4d
joseproldan <joseproldan@iesmontsia.org>
2023-12-18 16:44:41 +0100

    quitar espacios :D

--- DocumentRoot/api/main.py
+++ DocumentRoot/api/main.py
@@ -11,7 +11,6 @@ from celery.schedules import crontab
 import schedule
 import time
 
-
 # variable per guardar servei fast api
 app = FastAPI()
 
@@ -31,27 +30,25 @@ app.add_middleware(
     allow_headers=["*"],  # Puedes restringir a encabezados específicos si es necesario
 )
 
-
 def get_db_info():
     with open("config.json", "r") as config_file:
             config_data = json.load(config_file)
             db_config = config_data["database_config"]
             return db_config
-        
+
 def get_key_api():
     with open("config.json", "r") as keys_file:
             config_data = json.load(keys_file)
             api_key = config_data["parametres_seguretat"]["apy_keys"]
             return api_key
-        
+
 def get_info_picanova():
     with open("config.json", "r") as info_picanova:
             config_data = json.load(info_picanova)
             user_picanova = config_data["parametres_seguretat"]["info_picanova"]["user"]
             pass_picanova = config_data["parametres_seguretat"]["info_picanova"]["pass"]
             return [user_picanova,pass_picanova]
-        
-        
+
 def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
     if api_key_header in get_key_api():
         return api_key_header
@@ -67,12 +64,12 @@ def get_credentials():
     # Codificar las credenciales en base64
     credentials = base64.b64encode(f"{username}:{password}".encode()).decode()
     return credentials    
-    
+
 @app.get("/protected")
 def protected_route(api_key: str = Security(get_api_key)):
     # Process the request for authenticated usersresponse = httpx.get(url, headers=custom_headers)
     return {"message": "Access granted!"}
-    
+
 
 @app.get("/peticionotraapi")
 async def hacer_peticion(api_key: str = Security(get_api_key)):
@@ -91,62 +88,62 @@ async def hacer_peticion(api_key: str = Security(get_api_key)):
             #     names.append(item["data"])
             names = [item["name"] for item in data["data"]]
             return(names)
-        
+
         else:
             # Maneja los errores si la solicitud no fue exitosa
             return {"error": "No se pudo realizar la solicitud a la API externa"}
-        
+
 @app.get("/consultar_bd")
 async def consultar_bd(api_key: str = Security(get_api_key)):
-        
+
     try:
         # Conecta a la base de datos
         connection = mysql.connector.connect(**get_db_info())
-        
+
         # Crea un cursor para ejecutar consultas SQL
         cursor = connection.cursor(dictionary=True)
-        
+
         # Ejecuta una consulta
         query = "SELECT * FROM usuario"
         cursor.execute(query)
-        
+
         # Obtiene los resultados
         results = cursor.fetchall()
-        
+
         # Cierra el cursor y la conexión
         cursor.close()
         connection.close()
-        
+
         return results
     except Exception as e:
         return {"error": str(e)}
-   
+
 @app.get("/dbventes")
 async def consultar_bd(api_key: str = Security(get_api_key)):
-        
+
     try:
         # Conecta a la base de datos
         connection = mysql.connector.connect(**get_db_info())
-        
+
         # Crea un cursor para ejecutar consultas SQL
         cursor = connection.cursor(dictionary=True)
-        
+
         # Ejecuta una consulta
         query = "SELECT * FROM `vistaPedidosGrafica`"
         cursor.execute(query)
-        
+
         # Obtiene los resultados
         results = cursor.fetchall()
-        
+
         # Cierra el cursor y la conexión
         cursor.close()
         connection.close()
-        
+
         return results
     except Exception as e:
         return {"error": str(e)} 
 
-        
+
 @app.get("/test")
 def read_test():
     return {"La API funciona"}
@@ -171,25 +168,20 @@ async def hacer_peticion(api_key: str = Security(get_api_key)):
 
                 # Ahora, crearemos una lista de diccionarios con ambas claves
                 info_list = [{"country": item["name"], "countrycode": item.get("country_code", None)} for item in data_list]
-                
+
                 connection = mysql.connector.connect(**get_db_info())
                 # Crea un cursor para ejecutar consultas SQL
                 cursor = connection.cursor()
-                
-                            
+
                 query = "TRUNCATE TABLE `testdatabase2`.`pais`"
                 queryinsert = "INSERT INTO `pais`(`nombre_pais`,`codigo_pais`) VALUES (%s,%s);"
-                
+
                 cursor.execute(query)
-                
-                
                 for item in info_list:
                     country = item["country"]
                     countrycode = item["countrycode"]
                     cursor.execute(queryinsert, (country, countrycode))
-                
-                
-               
+
                 connection.commit()
                 cursor.close()
                 connection.close()
@@ -198,9 +190,8 @@ async def hacer_peticion(api_key: str = Security(get_api_key)):
             else:
                 # Maneja los errores si la solicitud no fue exitosa
                 return {"error": "No se pudo realizar la solicitud a la API externa"}
-    
+
     except mysql.connector.Error as e:
         return {"error": f"Error de MySQL: {e}"}
     except Exception as e:
         return {"error": f"Error no manejado: {e}"}
-
