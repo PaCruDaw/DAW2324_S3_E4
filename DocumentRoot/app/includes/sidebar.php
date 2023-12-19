fd64126c98bc6ae0bef2e538382e37deff542323
PaCruDaw <paulacruzado@iesmontsia.org>
2023-12-19 19:04:31 +0100

    acabada traduccio sidebar

--- DocumentRoot/app/includes/sidebar.php
+++ DocumentRoot/app/includes/sidebar.php
@@ -7,13 +7,12 @@
 <div class="d-flex justify-content-end mt-2">
     <div class="btn-group">
         <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-globe"></i> Idioma
+            <i class="fas fa-globe"></i> <?php echo TranslateTextPage::pageTranslate('Idiomas', 'sidebar', $lang);?>
         </button>
         <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#" onclick="cambiarIdioma(event,'esp')"><i class="fas fa-flag"></i> Español</a></li>
-            <li><a class="dropdown-item" href="#" onclick="cambiarIdioma(event,'eng')"><i class="fas fa-flag"></i> Inglés</a></li>
-            <li><a class="dropdown-item" href="#" onclick="cambiarIdioma(event,'cat')"><i class="fas fa-flag"></i> Catalán</a></li>
-            <li><a class="dropdown-item" href="#" onclick="cambiarIdioma(event,'ita')"><i class="fas fa-flag"></i> Italiano</a></li>
+            <li><a class="dropdown-item" href="#" onclick="cambiarIdioma(event,'esp')"><i class="fas fa-flag"></i> <?php echo TranslateTextPage::pageTranslate('Español', 'sidebar', $lang);?></a></li>
+            <li><a class="dropdown-item" href="#" onclick="cambiarIdioma(event,'eng')"><i class="fas fa-flag"></i><?php echo TranslateTextPage::pageTranslate('Inglés', 'sidebar', $lang);?></a></li>
+            <li><a class="dropdown-item" href="#" onclick="cambiarIdioma(event,'cat')"><i class="fas fa-flag"></i><?php echo TranslateTextPage::pageTranslate('Catalán', 'sidebar', $lang);?></a></li>
         </ul>
     </div>
 </div>
@@ -27,15 +26,15 @@
         <ul>
             <li><a href="../main.php"><i class="fas fa-home"></i><?php echo TranslateTextPage::pageTranslate('Inicio', 'sidebar', $lang);?></a></li>
             <li><a href="../vistas/vistatraducciones.php"><i class="fas fa-chart-bar"></i><?php echo TranslateTextPage::pageTranslate('Traducciones', 'sidebar', $lang);?></a></li>
-            <li><a href="../controladores/controladorcms.php"><i class="fas fa-users"></i> CMS</a></li>
-            <li><a href="../vistas/vistaEstadisticas.php"><i class="fas fa-cog"></i> Estadisticas</a></li>
-            <li><a href="#"><i class="fas fa-cog"></i> Paises</a></li>
-            <li><a href="../vistas/vistaProductos.php"><i class="fas fa-cog"></i> Productos/Margen</a></li>
-            <li><a href="../vistas/vistaPreferencias.php"><i class="fas fa-cog"></i> Preferencias</a></li>
+            <li><a href="../controladores/controladorcms.php"><i class="fas fa-users"></i> <?php echo TranslateTextPage::pageTranslate('CMS', 'sidebar', $lang);?></a></li>
+            <li><a href="../vistas/vistaEstadisticas.php"><i class="fas fa-cog"></i><?php echo TranslateTextPage::pageTranslate('Estadisticas', 'sidebar', $lang);?></a></li>
+            <li><a href="#"><i class="fas fa-cog"></i><?php echo TranslateTextPage::pageTranslate('Paises', 'sidebar', $lang);?></a></li>
+            <li><a href="../vistas/vistaProductos.php"><i class="fas fa-cog"></i><?php echo TranslateTextPage::pageTranslate('Productos/Margen', 'sidebar', $lang);?></a></li>
+            <li><a href="../vistas/vistaPreferencias.php"><i class="fas fa-cog"></i><?php echo TranslateTextPage::pageTranslate('Preferencias', 'sidebar', $lang);?></a></li>
             <li><a href="../vistas/vistaUsuarios.php"><i class="fas fa-cog"></i><?php echo TranslateTextPage::pageTranslate('Usuarios', 'sidebar', $lang);?></a></li>
         </ul>
         <section class="logout">
-            <a href="../index.php"><i class="fas fa-cog"></i> LOG OUT</a>
+            <a href="../index.php"><i class="fas fa-cog"></i><?php echo TranslateTextPage::pageTranslate('Cerrar sesión', 'sidebar', $lang);?></a>
         </section>
     </div>
     <script src="../js/sidebar.js" rel="script"></script>
