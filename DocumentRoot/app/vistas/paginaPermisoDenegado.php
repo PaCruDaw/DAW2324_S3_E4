6e0e2121009106ab301077511b0dd4e5ff30bd80
michipce <michipce@gmail.com>
2024-01-07 23:06:24 +0100

    pagina acceso denegado

---
+++ DocumentRoot/app/vistas/paginaPermisoDenegado.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilos/paginaPermisoDenegado.css">
+    <title>No tienes permisos</title>
+</head>
+<body>
+    <div class="container">
+        <div class="illustration"></div>
+    </div>
+</body>
+</html>
