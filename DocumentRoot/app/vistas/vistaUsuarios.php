0fd99f382b2f9ac9ef45bce9c28dd5df506b0a91
PaCruDaw <paulacruzado@iesmontsia.org>
2024-01-09 18:36:08 +0100

    sidebar

--- DocumentRoot/app/vistas/vistaUsuarios.php
+++ DocumentRoot/app/vistas/vistaUsuarios.php
@@ -11,7 +11,6 @@ include '../controladores/controladorFormUsuaris.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="../js/tablaformusuarios.js" rel="script"></script>
     <title>Administrador de Usuarios</title>
-    <?php include('../includes/sidebar.php'); ?>
 </head>
 <style>
         body {
